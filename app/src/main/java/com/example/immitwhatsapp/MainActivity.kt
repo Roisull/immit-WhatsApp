72d849c11093e08059efb643a82e9d8a63b0aad3
maulana421 <maulanaibnuf@gmaile.com>
2022-09-04 10:22:08 +0700

    viewpager_lana

--- app/src/main/java/com/example/immitwhatsapp/MainActivity.kt
+++ app/src/main/java/com/example/immitwhatsapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.immitwhatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.immitwhatsapp.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewPageAdapter: ViewPageAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewPageAdapter = ViewPageAdapter(supportFragmentManager, lifecycle)
+
+        with(binding){
+            viewPager.adapter = viewPageAdapter
+
+            TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+                when(position){
+                    0 -> tab.text = "Chat"
+                    1 -> tab.text = "Status"
+                    2 -> tab.text = "Call"
+                }
+            }.attach()
+        }
     }
 }
---
+++ app/src/main/java/com/example/immitwhatsapp/ViewPageAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.immitwhatsapp
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int = 3
+
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment = Fragment()
+        when(position){
+            0 -> fragment = ChatFragment()
+            1 -> fragment = StatusFragment()
+            2 -> fragment = CallFragment()
+        }
+        return fragment
+    }
+}
